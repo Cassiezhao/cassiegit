950135f1d07f9faecb5560e98a0fe0df1e5db452
cassiezhao <ylvh1314@163.com>
2016-05-11 09:49:04 +0800

    add 2 py

---
+++ calc.py
@@ -0,0 +1,17 @@
+print(100+200+300)
+print('please input a name')
+name = input()
+print('hello',name)
+print('please enter a number')
+s=input('age:')
+age=int(s)
+if age >= 18:
+    print('your age is',age)
+    print('adult')
+elif age>=6:
+    print('your age is',age) 
+    print('teeneager')
+else:
+    print('your age is',age)
+    print('kid')
+
