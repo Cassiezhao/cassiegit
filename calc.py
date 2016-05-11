32219951601906ef09d06b549ca90d7ba366c79a
cassiezhao <ylvh1314@163.com>
2016-05-11 10:28:19 +0800

    modify

--- calc.py
+++ calc.py
@@ -14,4 +14,29 @@ elif age>=6:
 else:
     print('your age is',age)
     print('kid')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+# 打印list:
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+
+# 打印数字 0 - 9
+for x in range(10):
+    print(x)
+# 计算1+2+3+...+100:
+sum = 0
+n = 1
+while n <= 100:
+    sum = sum + n
+    n = n + 1
+print(sum)
+
+# 计算1x2x3x...x100:
+acc = 1
+n = 1
+while n <= 100:
+    acc = acc * n
+    n = n + 1
+print(acc)
