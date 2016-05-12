d97d42fd0fc676726c43f4fa43c049cdfbeb24e2
cassiezhao <ylvh1314@163.com>
2016-05-12 16:58:45 +0800

    add 2 py

---
+++ 1.py
@@ -0,0 +1,14 @@
+#!/home/cassiezhao/cassiegit
+
+def fun():
+    return 0
+
+x=int(raw_input("please input x:"))
+y=int(raw_input("please input y:"))
+
+#if x >= 90 and y >= 90:
+if not fun():
+    print"ok"
+else:
+    print "bad"
+
---
+++ 2.py
@@ -0,0 +1,5 @@
+#!/home/cassiezhao/cassiegit
+sum=0
+for x in range(1,101):
+     sum+=x
+print sum
