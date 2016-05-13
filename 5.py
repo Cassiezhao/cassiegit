6130028c013a2743b2f740f5a95c32644e7417a8
cassiezhao <ylvh1314@163.com>
2016-05-13 14:00:14 +0800

    add 5.py

---
+++ 5.py
@@ -0,0 +1,13 @@
+#!/home/cassiezhao/cassiegit
+x=""
+
+while x != "q":
+    print "hello"
+    x=raw_input("please input something,q for quit:")
+    if not x :
+        break
+    if x == "c":
+    	continue
+    print "one more time ...."
+else:
+        print "ending...."
